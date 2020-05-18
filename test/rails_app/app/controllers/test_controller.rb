4f3419d03dad08b8d7c5353b0dbad782e0dc9988
HoneyryderChuck <cardoso_tiago@hotmail.com>
2020-05-18 18:00:30 +0100

    added more gluer code to the rails application

---
+++ test/rails_app/app/controllers/test_controller.rb
@@ -0,0 +1,16 @@
+class TestController < ApplicationController
+  def root
+  	render inline: flash[:alert] || flash[:notice] || "Unauthorized"
+  end
+
+  def callback
+    render inline: "Callback" 
+  end
+
+  def private
+    rodauth.require_authentication
+    rodauth.require_oauth_authorization
+
+    flash["error"] || flash["notice"] || "Authorized"
+  end
+end
--- test/rails_app/lib/rodauth_app.rb
+++ test/rails_app/lib/rodauth_app.rb
@@ -1,58 +1,21 @@
 class RodauthApp < Rodauth::Rails::App
   configure do
-    enable :create_account, :verify_account, :verify_account_grace_period,
-      :login, :remember, :logout,
-      :reset_password, :change_password, :change_password_notify,
-      :change_login, :verify_login_change,
-      :close_account, :lockout
-
-    if Rodauth::MAJOR == 2
-      enable :active_sessions
-    end
+    enable :login, :http_basic_auth, :oauth 
 
     rails_controller { RodauthController }
 
-    account_status_column :status
-    account_unverified_status_value "unverified"
-    account_open_status_value "verified"
-    account_closed_status_value "closed"
-
-    email_subject_prefix "[RodauthTest] "
-    email_from "noreply@rodauth.test"
-
-    require_login_confirmation? false
-    verify_account_set_password? false
-    extend_remember_deadline? true
-    max_invalid_logins 3
-
-    after_login { remember_login }
-
-    logout_redirect "/"
-    verify_account_redirect { login_redirect }
-    reset_password_redirect { login_path }
+    skip_status_checks? true
+    password_match? do |_password|
+      true
+    end
 
     # OAuth
-
-    enable :oauth
     oauth_application_default_scope TEST_SCOPES.first
     oauth_application_scopes TEST_SCOPES
   end
 
-  configure(:admin) do
-    prefix "/admin"
-  end
-
   route do |r|
-    rodauth.load_memory
-
     r.rodauth
-
-    r.on "admin" do
-      r.rodauth(:admin)
-    end
-
-    if r.path == "/auth1"
-      rodauth.require_authentication
-    end
+    rodauth.oauth_applications
   end
 end
