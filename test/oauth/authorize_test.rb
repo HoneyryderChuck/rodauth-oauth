ce53107c72dd78914e1992be37bc1d85144683e3
HoneyryderChuck <cardoso_tiago@hotmail.com>
2020-05-27 14:36:32 +0100

    fixed implicit grant test, i.e. response_type will be validated on GET as well

--- test/oauth/authorize_test.rb
+++ test/oauth/authorize_test.rb
@@ -121,11 +121,6 @@ class RodaOauthAuthorizeTest < RodaIntegration
 
     # show the authorization form
     visit "/oauth-authorize?client_id=#{oauth_application[:client_id]}&scopes=user.read+user.write&response_type=token"
-    assert page.current_path == "/oauth-authorize",
-           "was redirected instead to #{page.current_path}"
-
-    # submit authorization request
-    click_button "Authorize"
 
     assert page.current_url.include?("?error=invalid_request"),
            "was redirected instead to #{page.current_url}"
@@ -140,7 +135,7 @@ class RodaOauthAuthorizeTest < RodaIntegration
     login
 
     # show the authorization form
-    visit "/oauth-authorize?client_id=#{oauth_application[:client_id]}&&response_type=token"
+    visit "/oauth-authorize?client_id=#{oauth_application[:client_id]}&response_type=token"
     assert page.current_path == "/oauth-authorize",
            "was redirected instead to #{page.current_path}"
 
