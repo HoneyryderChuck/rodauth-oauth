42a2c1b8f98dd0a8d49e6c1a559797134e3c6869
HoneyryderChuck <cardoso_tiago@hotmail.com>
2020-07-11 03:11:07 +0100

    loading oidc attributes from scopes
    * using get_oidc_param abstract method (user has to redefine it)
    * giving proper visual info to the user on what to do

--- lib/rodauth/features/oidc.rb
+++ lib/rodauth/features/oidc.rb
@@ -2,6 +2,14 @@
 
 module Rodauth
   Feature.define(:oidc) do
+    OIDC_SCOPES_MAP = {
+      "profile" => %i[name family_name given_name middle_name nickname preferred_username
+                      profile picture website gender birthdate zoneinfo locale updated_at].freeze,
+      "email" => %i[email email_verified].freeze,
+      "address" => %i[address].freeze,
+      "phone" => %i[phone_number phone_number_verified].freeze
+    }.freeze
+
     depends :oauth_jwt
 
     auth_value_method :oauth_application_default_scope, "openid"
@@ -9,6 +17,8 @@ module Rodauth
 
     auth_value_method :oauth_grants_nonce_column, :nonce
     auth_value_method :oauth_tokens_nonce_column, :nonce
+
+    auth_value_methods(:get_oidc_param)
 
     private
 
@@ -27,7 +37,9 @@ module Rodauth
     def create_oauth_token
       oauth_token = super
 
-      return oauth_token unless oauth_token[oauth_tokens_scopes_column].split(oauth_scope_separator).include?("openid")
+      oauth_scopes = oauth_token[oauth_tokens_scopes_column].split(oauth_scope_separator)
+
+      return oauth_token unless oauth_scopes.include?("openid")
 
       id_token_claims = jwt_claims(oauth_token)
       id_token_claims[:nonce] = oauth_token[oauth_tokens_nonce_column] if oauth_token[oauth_tokens_nonce_column]
@@ -37,8 +49,21 @@ module Rodauth
       # Sounds like the same as issued at claim.
       id_token_claims[:auth_time] = id_token_claims[:iat]
 
-      oauth_token[:id_token] = jwt_encode(id_token_claims)
+      oidc_scopes = (OIDC_SCOPES_MAP.keys & oauth_scopes)
 
+      unless oidc_scopes.empty?
+        if respond_to?(:get_oidc_param)
+          oidc_scopes.each do |scope|
+            OIDC_SCOPES_MAP[scope].each do |param|
+              id_token_claims[param] = __send__(:get_oidc_param, oauth_token, param)
+            end
+          end
+        else
+          warn "`get_oidc_param(token, param)` must be implemented to use oidc scopes."
+        end
+      end
+
+      oauth_token[:id_token] = jwt_encode(id_token_claims)
       oauth_token
     end
 
