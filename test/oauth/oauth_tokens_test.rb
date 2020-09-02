9d5ec2064074429c5b7ad843adaebf237b186a3d
HoneyryderChuck <cardoso_tiago@hotmail.com>
2020-09-02 01:16:54 +0100

    testing admin oauth tokens page flow; found out some vugs in the views used for tests

--- test/oauth/oauth_tokens_test.rb
+++ test/oauth/oauth_tokens_test.rb
@@ -3,31 +3,31 @@
 require "test_helper"
 
 class RodauthOauthTokensTest < RodaIntegration
-  # def test_oauth_tokens
-  #   setup_application
-  #   login
-  #   # List
-  #   visit "/oauth-applications/#{oauth_application[:id]}/oauth-tokens"
-  #   assert_includes page.html, "No oauth tokens yet!"
+  def test_oauth_tokens
+    setup_application
+    login
+    # List
+    visit "/oauth-applications/#{oauth_application[:id]}/oauth-tokens"
+    assert_includes page.html, "No oauth tokens yet!"
 
-  #   oauth_token
-  #   visit "/oauth-applications/#{oauth_application[:id]}/oauth-tokens"
+    oauth_token
+    visit "/oauth-applications/#{oauth_application[:id]}/oauth-tokens"
 
-  #   assert_includes page.html, oauth_token[:token]
-  #   assert_includes page.html, oauth_token[:refresh_token]
-  #   assert_includes page.html, oauth_token[:expires_in].to_s
-  #   assert_includes page.html, "value=\"Revoke"
+    assert_includes page.html, oauth_token[:token]
+    assert_includes page.html, oauth_token[:refresh_token]
+    assert_includes page.html, oauth_token[:expires_in].to_s
+    assert_includes page.html, "value=\"Revoke"
 
-  #   # Revokes token
-  #   click_button "Revoke"
-  #   assert_equal page.find("#notice_flash").text, "The oauth token has been revoked"
-  #   assert_includes page.html, oauth_token[:token]
-  #   assert_includes page.html, oauth_token[:refresh_token]
-  #   assert_includes page.html, oauth_token[:expires_in].to_s
-  #   refute_includes page.html, "value=\"Revoke"
+    #   # Revokes token
+    #   click_button "Revoke"
+    #   assert_equal page.find("#notice_flash").text, "The oauth token has been revoked"
+    #   assert_includes page.html, oauth_token[:token]
+    #   assert_includes page.html, oauth_token[:refresh_token]
+    #   assert_includes page.html, oauth_token[:expires_in].to_s
+    #   refute_includes page.html, "value=\"Revoke"
 
-  #   assert db[:oauth_tokens].where(revoked_at: nil).count.zero?
-  # end
+    #   assert db[:oauth_tokens].where(revoked_at: nil).count.zero?
+  end
 
   private
 
