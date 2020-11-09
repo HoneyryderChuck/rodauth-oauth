dbb07b7d20824f81c23b1ebefb7f2832f164366a
HoneyryderChuck <cardoso_tiago@hotmail.com>
2020-11-09 22:23:55 +0000

    adding account statuses to rails migrations as well

--- test/rails_app/db/migrate/001_create_rodauth.rb
+++ test/rails_app/db/migrate/001_create_rodauth.rb
@@ -12,9 +12,23 @@ class CreateRodauth < superclass
   self.verbose = false
 
   def change
+    unless table_exists?(:account_statuses)
+      create_table :account_statuses do |t|
+        t.string :name, null: false, index: { unique: true }
+      end
+      execute <<-SQL
+  INSERT INTO account_statuses (id, name) values
+  (1, 'Unverified'),
+  (2, 'Verified'),
+  (3, 'Closed')
+      SQL
+    end
+
+
     create_table :accounts do |t|
       t.string :email, null: false, index: { unique: true }
       t.string :ph
+      t.integer :status_id, references: :account_statuses, null: false, default: 1
     end unless table_exists?(:accounts)
   end
 end
