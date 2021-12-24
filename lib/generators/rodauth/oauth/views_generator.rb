727847a7c2a281fc7a895167b33665aed3052a0d
HoneyryderChuck <cardoso_tiago@hotmail.com>
2021-12-24 12:49:06 +0200

    fixed generator declarations and views generator, which must copy files and rewrite paths

--- lib/generators/rodauth/oauth/views_generator.rb
+++ lib/generators/rodauth/oauth/views_generator.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "rails/generators/base"
+require "rails/generators"
 
 module Rodauth::OAuth
   module Rails
@@ -8,6 +8,7 @@ module Rodauth::OAuth
       class ViewsGenerator < ::Rails::Generators::Base
         source_root "#{__dir__}/templates"
         namespace "rodauth:oauth:views"
+        desc "Generate db migrations for rodauth-oauth in your application."
 
         DEFAULT = %w[authorize].freeze
         VIEWS = {
@@ -31,16 +32,20 @@ module Rodauth::OAuth
                                  default: "rodauth"
 
         def create_views
-          features = options[:all] ? VIEWS.keys : (DEFAULT + options[:features]).map(&:to_sym)
+          features = options[:all] ? VIEWS.keys : (%i[oauth_authorize] + options[:features]).map(&:to_sym).uniq
 
           views = features.inject([]) do |list, feature|
             list |= VIEWS[feature] || []
             list |= VIEWS[DEPENDENCIES[feature]] || []
           end
 
+          directory = options[:directory].underscore
           views.each do |view|
-            template "app/views/rodauth/#{view}.html.erb",
-                     "app/views/#{options[:directory].underscore}/#{view}.html.erb"
+            copy_file "app/views/rodauth/#{view}.html.erb",
+                     "app/views/#{directory}/#{view}.html.erb" do |content|
+              content = content.gsub("rodauth/", "#{directory}/")
+              content
+            end
           end
         end
       end
