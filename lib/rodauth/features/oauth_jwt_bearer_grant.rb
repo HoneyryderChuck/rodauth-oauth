89c331a238d5bc7e2f8664d66eeed9905c8d42bd
HoneyryderChuck <cardoso_tiago@hotmail.com>
2022-02-25 00:22:35 +0000

    fix:jwt-bearer now works and tests against self-issued client authentication assertions

--- lib/rodauth/features/oauth_jwt_bearer_grant.rb
+++ lib/rodauth/features/oauth_jwt_bearer_grant.rb
@@ -28,15 +28,35 @@ module Rodauth
 
     def require_oauth_application
       # requset authentication optional for assertions
-      return super unless use_oauth_jwt_bearer_grant_type? && param("grant_type") == "urn:ietf:params:oauth:grant-type:jwt-bearer"
+      return super unless use_oauth_jwt_bearer_grant_type?
 
-      claims = jwt_decode(param("assertion"))
+      if (assertion = param_or_nil("assertion"))
 
-      redirect_response_error("invalid_grant") unless claims
+        claims = jwt_decode(assertion)
 
-      @oauth_application = db[oauth_applications_table].where(oauth_applications_client_id_column => claims["client_id"]).first
+        redirect_response_error("invalid_grant") unless claims
 
-      authorization_required unless @oauth_application
+        @oauth_application = db[oauth_applications_table].where(
+          oauth_applications_client_id_column => param_or_nil("client_id") || claims["client_id"]
+        ).first
+
+        authorization_required unless @oauth_application
+      elsif param("client_assertion_type") == "urn:ietf:params:oauth:grant-type:jwt-bearer" &&
+            (assertion = param_or_nil("client_assertion"))
+
+        claims = jwt_decode(assertion)
+
+        redirect_response_error("invalid_grant") unless claims
+
+        #  When using assertions for client authentication, the Subject
+        # identifies the client to the authorization server using the
+        # value of the "client_id" of the OAuth client.
+        @oauth_application = db[oauth_applications_table].where(oauth_applications_client_id_column => claims["sub"]).first
+
+        authorization_required unless @oauth_application
+      else
+        super
+      end
     end
 
     def create_oauth_token_from_assertion
@@ -48,7 +68,9 @@ module Rodauth
 
       create_params = {
         oauth_tokens_account_id_column => claims["sub"],
-        oauth_tokens_oauth_application_id_column => oauth_application[oauth_applications_id_column],
+        oauth_tokens_oauth_application_id_column => db[oauth_applications_table].where(
+          oauth_applications_client_id_column => claims["client_id"]
+        ).select(oauth_applications_id_column),
         oauth_tokens_scopes_column => claims["scope"]
       }
 
--- test/jwt/token_jwt_bearer_test.rb
+++ test/jwt/token_jwt_bearer_test.rb
@@ -5,7 +5,7 @@ require "test_helper"
 class RodauthOauthJWTTokenJwtBearerTest < JWTIntegration
   include Rack::Test::Methods
 
-  def test_oauth_jwt_as_authorization_grant
+  def test_oauth_jwt_bearer_as_authorization_grant
     rodauth do
       use_oauth_jwt_bearer_grant_type? true
       oauth_jwt_key "SECRET"
@@ -45,9 +45,61 @@ class RodauthOauthJWTTokenJwtBearerTest < JWTIntegration
     assert last_response.status == 200
   end
 
+  def test_oauth_jwt_bearer_as_client_authentication_behalf_of_itself
+    rodauth do
+      use_oauth_jwt_bearer_grant_type? true
+      oauth_jwt_key "SECRET"
+      oauth_jwt_algorithm "HS256"
+    end
+    setup_application
+
+    assertion = generate_jwt("HS256", "SECRET", sub: oauth_application[:client_id])
+
+    post("/token",
+         client_assertion_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
+         client_assertion: assertion,
+         client_id: oauth_application[:client_id],
+         client_secret: "CLIENT_SECRET",
+         grant_type: "authorization_code",
+         code: oauth_grant[:code],
+         redirect_uri: oauth_grant[:redirect_uri])
+
+    verify_response
+
+    jwt_token = json_body["access_token"]
+
+    # use token
+    header "Authorization", "Bearer #{jwt_token}"
+
+    # valid token, and now we're getting somewhere
+    get("/private")
+    assert last_response.status == 200
+  end
+
   private
 
   def oauth_feature
     :oauth_jwt_bearer_grant
+  end
+
+  def generate_jwt(algo, signing_key, extra_claims = {})
+    claims = {
+      iss: "Example",
+      client_id: oauth_application[:client_id],
+      aud: "Audience",
+      sub: account[:id],
+      iat: Time.now.to_i, # issued at
+      exp: Time.now.to_i + 3600
+    }.merge(extra_claims)
+    claims[:jti] = Digest::SHA256.hexdigest("#{claims[:aud]}:#{claims[:iat]}")
+
+    headers = {}
+
+    jwk = JWT::JWK.new(signing_key)
+    headers[:kid] = jwk.kid
+
+    signing_key = jwk.keypair
+
+    JWT.encode(claims, signing_key, algo, headers)
   end
 end
