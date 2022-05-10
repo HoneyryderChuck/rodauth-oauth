3c4de408e5f521f967e9697bcd760c86d229535a
HoneyryderChuck <cardoso_tiago@hotmail.com>
2022-05-10 16:20:44 +0100

    added missing jwt public key param

--- lib/rodauth/features/oauth_jwt.rb
+++ lib/rodauth/features/oauth_jwt.rb
@@ -38,6 +38,7 @@ module Rodauth
     auth_value_method :oauth_applications_request_object_encryption_enc_column, :request_object_encryption_enc
 
     translatable_method :oauth_applications_jwt_public_key_label, "Public key"
+    auth_value_method :oauth_application_jwt_public_key_param, "jwt_public_key"
 
     auth_value_method :oauth_jwt_keys, {}
     auth_value_method :oauth_jwt_key, nil
