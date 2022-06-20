HoneyryderChuck/rodauth-oauth

commit 83e3f183f6c9941d37c8fe8cfd3fc258ab9c576a

rotation policy will now update the db entry with both token and refresh token
previously, due to the fixed bug with missing account id, it was
possible to have multiple entries with the single composite index on
application, account and scopes. so now, the way forward is to update
the entry.

lib/rodauth/features/oauth_base.rb
@@ -587,28 +587,19 @@ module Rodauth
         oauth_tokens_ds = db[oauth_tokens_table]
         access_token = _generate_access_token(update_params)
 
-        oauth_token = if oauth_refresh_token_protection_policy == "rotation"
-                        insert_params = {
-                          **update_params,
-                          oauth_tokens_oauth_token_id_column => oauth_token[oauth_tokens_id_column],
-                          oauth_tokens_account_id_column => oauth_token[oauth_tokens_account_id_column],
-                          oauth_tokens_scopes_column => oauth_token[oauth_tokens_scopes_column]
-                        }
+        if oauth_refresh_token_protection_policy == "rotation"
+          update_params = {
+            **update_params,
+            oauth_tokens_oauth_token_id_column => oauth_token[oauth_tokens_id_column],
+            oauth_tokens_account_id_column => oauth_token[oauth_tokens_account_id_column],
+            oauth_tokens_scopes_column => oauth_token[oauth_tokens_scopes_column]
+          }
 
-                        refresh_token = _generate_refresh_token(insert_params)
-
-                        # revoke the refresh token
-                        oauth_tokens_ds.where(oauth_tokens_id_column => oauth_token[oauth_tokens_id_column])
-                                       .update(oauth_tokens_revoked_at_column => Sequel::CURRENT_TIMESTAMP)
-
-                        insert_params[oauth_tokens_oauth_token_id_column] = oauth_token[oauth_tokens_id_column]
-                        __insert_and_return__(oauth_tokens_ds, oauth_tokens_id_column, insert_params)
-                      else
-                        refresh_token = param("refresh_token")
-                        # includes none
-                        ds = oauth_tokens_ds.where(oauth_tokens_id_column => oauth_token[oauth_tokens_id_column])
-                        __update_and_return__(ds, update_params)
-                      end
+          refresh_token = _generate_refresh_token(update_params)
+        else
+          refresh_token = param("refresh_token")
+        end
+        oauth_token = __update_and_return__(oauth_tokens_ds, update_params)
 
         oauth_token[oauth_tokens_token_column] = access_token
         oauth_token[oauth_tokens_refresh_token_column] = refresh_token

test/jwt/refresh_token_grant/token_refresh_token_test.rb
@@ -127,8 +127,7 @@ class RodauthOAuthJWTRefreshTokenTest < JWTIntegration
          grant_type: "refresh_token",
          refresh_token: oauth_token[:refresh_token])
 
-    assert last_response.status == 200
-    assert last_response.headers["Content-Type"] == "application/json"
+    verify_response
 
     assert db[:oauth_tokens].where(revoked_at: nil).count == 1
 
@@ -152,22 +151,14 @@ class RodauthOAuthJWTRefreshTokenTest < JWTIntegration
          grant_type: "refresh_token",
          refresh_token: oauth_token[:refresh_token])
 
-    assert last_response.status == 200
-    assert last_response.headers["Content-Type"] == "application/json"
-
-    # previous token gets revoked
-    assert db[:oauth_tokens].count == 2
-    assert db[:oauth_tokens].where(revoked_at: nil).count == 1
-
-    new_token = db[:oauth_tokens].where(revoked_at: nil).first
-    assert new_token[:access_token] == json_body["token"]
-    assert new_token[:refresh_token] == json_body["refresh_token"]
-
+    verify_response
     verify_refresh_token_response(json_body, oauth_token)
     assert json_body["refresh_token"] != oauth_token[:refresh_token]
-    token = json_body["access_token"]
 
-    verify_access_token_response(json_body.merge("access_token" => token), oauth_token, "SECRET", "HS256")
+    # previous token gets revoked
+    assert db[:oauth_tokens].count == 1
+
+    verify_access_token_response(json_body, oauth_token, "SECRET", "HS256")
 
     # invalidates all tokens generated from that token
     @json_body = nil
@@ -181,8 +172,6 @@ class RodauthOAuthJWTRefreshTokenTest < JWTIntegration
     assert last_response.headers["Content-Type"] == "application/json"
     assert json_body["error"] == "invalid_grant"
     assert json_body["error_description"] == "Invalid grant"
-
-    assert db[:oauth_tokens].where(revoked_at: nil).count.zero?
   end
 
   private

test/oauth/refresh_token_grant/token_refresh_token_test.rb
@@ -167,17 +167,11 @@ class RodauthOAuthRefreshTokenTest < RodaIntegration
 
     assert last_response.status == 200
     assert last_response.headers["Content-Type"] == "application/json"
-
-    # previous token gets revoked
-    assert db[:oauth_tokens].count == 2
-    assert db[:oauth_tokens].where(revoked_at: nil).count == 1
-
-    new_token = db[:oauth_tokens].where(revoked_at: nil).first
-    assert new_token[:access_token] == json_body["token"]
-    assert new_token[:refresh_token] == json_body["refresh_token"]
-
     verify_refresh_token_response(json_body, oauth_token)
     assert json_body["refresh_token"] != oauth_token[:refresh_token]
+
+    # previous token gets updated
+    assert db[:oauth_tokens].count == 1
 
     # invalidates all tokens generated from that token
     @json_body = nil
@@ -191,8 +185,6 @@ class RodauthOAuthRefreshTokenTest < RodaIntegration
     assert last_response.headers["Content-Type"] == "application/json"
     assert json_body["error"] == "invalid_grant"
     assert json_body["error_description"] == "Invalid grant"
-
-    assert db[:oauth_tokens].where(revoked_at: nil).count.zero?
   end
 
   private

test/oidc/refresh_token_grant/token_refresh_token_test.rb
@@ -123,19 +123,12 @@ class RodauthOAuthOIDCRefreshTokenTest < OIDCIntegration
          grant_type: "refresh_token",
          refresh_token: oauth_token[:refresh_token])
 
-    assert last_response.status == 200
-    assert last_response.headers["Content-Type"] == "application/json"
-
-    # previous token gets revoked
-    assert db[:oauth_tokens].count == 2
-    assert db[:oauth_tokens].where(revoked_at: nil).count == 1
-
-    new_token = db[:oauth_tokens].where(revoked_at: nil).first
-    assert new_token[:access_token] == json_body["token"]
-    assert new_token[:refresh_token] == json_body["refresh_token"]
-
+    verify_response
     verify_refresh_token_response(json_body, oauth_token)
     assert json_body["refresh_token"] != oauth_token[:refresh_token]
+
+    # previous token gets revoked
+    assert db[:oauth_tokens].count == 1
 
     verify_access_token_response(json_body, oauth_token, "SECRET", "HS256")
 
@@ -151,8 +144,6 @@ class RodauthOAuthOIDCRefreshTokenTest < OIDCIntegration
     assert last_response.headers["Content-Type"] == "application/json"
     assert json_body["error"] == "invalid_grant"
     assert json_body["error_description"] == "Invalid grant"
-
-    assert db[:oauth_tokens].where(revoked_at: nil).count.zero?
   end
 
   private

commit 58483cca95666d13595d8830d9b76468d53bf13e

bugfix: scope by id when refreshing the token
bug introduced in commit 83e3f183f6c9941d37c8fe8cfd3fc258ab9c576a ,
where this logic was refactored. tests didn't get it due to it being the
only token in the db.

lib/rodauth/features/oauth_base.rb
@@ -589,7 +589,7 @@ module Rodauth
       redirect_response_error("invalid_grant") unless token_from_application?(oauth_token, oauth_application)
 
       rescue_from_uniqueness_error do
-        oauth_tokens_ds = db[oauth_tokens_table]
+        oauth_tokens_ds = db[oauth_tokens_table].where(oauth_tokens_id_column => oauth_token[oauth_tokens_id_column])
         access_token = _generate_access_token(update_params)
 
         if oauth_refresh_token_protection_policy == "rotation"