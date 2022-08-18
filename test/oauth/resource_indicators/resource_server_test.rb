c3c460456b6145cf54930e0d73d3ce0b99a97818
HoneyryderChuck <cardoso_tiago@hotmail.com>
2022-08-18 18:28:24 +0100

    take auth server url path into consideration, if servers have a '/oauth' suffix

--- test/oauth/resource_indicators/resource_server_test.rb
+++ test/oauth/resource_indicators/resource_server_test.rb
@@ -52,14 +52,32 @@ class RodauthOAuthResourceIndicatorsResourceServerTest < RodaIntegration
     assert last_response.status == 200
   end
 
+  def test_token_access_private_valid_auth_server_with_path
+    setup_application("https://auth-server/oauth")
+
+    header "Accept", "application/json"
+    header "Authorization", "Bearer TOKEN"
+    stub_request(:post, "https://auth-server/oauth/introspect")
+      .with(body: "token_type_hint=access_token&token=TOKEN")
+      .to_return(body: JSON.dump({
+                                   "scope" => "profile.read",
+                                   "active" => true,
+                                   "client_id" => "CLIENT_ID",
+                                   "aud" => %w[http://example.org]
+                                 }))
+    # valid token, and now we're getting somewhere
+    get("/private")
+    assert last_response.status == 200
+  end
+
   private
 
-  def setup_application
+  def setup_application(auth_url = "https://auth-server")
     resource_server = Class.new(Roda)
     resource_server.plugin :rodauth do
       enable :oauth, :oauth_resource_indicators
       is_authorization_server? false
-      authorization_server_url "https://auth-server"
+      authorization_server_url auth_url
     end
     resource_server.route do |r|
       rodauth.require_oauth_authorization("profile.read")
